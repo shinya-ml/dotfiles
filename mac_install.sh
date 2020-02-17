5592299b3b26be41b13bbe5626dfc460b90cc945
shinya-ml <epsonia_ss08.icloud.com>
2020-02-17 14:24:31 +0900

    fix

--- mac_install.sh
+++ mac_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # xcodeをapp storeからインストールしておく
-xcode-select -install
+xcode-select --install
 #homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
